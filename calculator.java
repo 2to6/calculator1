346cc8b6ab4bec99e6f23e0770b20941439ebbff
do02reen24 <do02reen24@gmail.com>
2019-07-03 16:47:41 +0900

    ADD calculator MENU

--- calculator.java
+++ calculator.java
@@ -2,6 +2,12 @@ public class calculator
 {
 	public static void main(String argv[])
 	{
-		
+		System.out.println("======MENU=======");
+		System.out.println("1. 더하기");
+		System.out.println("2. 빼기");
+		System.out.println("3. 곱하기");
+		System.out.println("4. 나누기");
+		System.out.println("5. 제곱");
+		System.out.println("=================");
 	}
 }

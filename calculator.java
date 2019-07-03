c9083cedd211150cf99041b102aca6fcc8dcd25c
do02reen24 <do02reen24@gmail.com>
2019-07-03 16:51:14 +0900

    ADD menu case

--- calculator.java
+++ calculator.java
@@ -2,13 +2,39 @@ public class calculator
 {
 	public static void main(String argv[])
 	{
-		System.out.println("======MENU=======");
-		System.out.println("1. 더하기");
-		System.out.println("2. 빼기");
-		System.out.println("3. 곱하기");
-		System.out.println("4. 나누기");
-		System.out.println("5. 제곱");
-		System.out.println("0. 시스템 종료");
-		System.out.println("=================");
+		while(1){
+			System.out.println("======MENU=======");
+			System.out.println("1. 더하기");
+			System.out.println("2. 빼기");
+			System.out.println("3. 곱하기");
+			System.out.println("4. 나누기");
+			System.out.println("5. 제곱");
+			System.out.println("0. 시스템 종료");
+			System.out.println("=================");
+			int menu;
+			switch (menu) {
+				case 1:
+					
+					break;
+				case 2:
+					
+					break;
+				case 3:
+					
+					break;
+				case 4:
+					
+					break;
+				case 5:
+					
+					break;
+				case 6:
+					System.out.println("시스템을 종료합니다.");
+					return;
+				default:
+					System.out.println("잘못된 입력입니다.");
+					break;
+			}
+		}
 	}
 }

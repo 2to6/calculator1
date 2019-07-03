ba8d5989dcd4ef546a17397b8bb6802912349bf1
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-03 17:06:01 +0900

    Add Exception Handling

--- calculator.java
+++ calculator.java
@@ -4,7 +4,15 @@ public class calculator
 	{
 		public double div(double a, double b){
 			double result;
+
+			try{
 				result = a/b;
+			}
+			catch(Exception e){
+				System.err.println("You can't divide by zero");
+				result = 0;
+			}
+
 			return result;
 		}
 	}

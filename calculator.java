b8b860ade6de303e04bc8affdd873f49bbd31b45
Choejiwon <jiwon0929@ajou.ac.kr>
2019-07-03 17:03:09 +0900

    Add Basic Divide function

--- calculator.java
+++ calculator.java
@@ -2,6 +2,11 @@ public class calculator
 {
 	public static void main(String argv[])
 	{
-		
+		public double div(double a, double b){
+			double result;
+				result = a/b;
+			return result;
+		}
 	}
+
 }

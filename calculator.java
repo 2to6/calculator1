5f13532d42d9cc5e9703d80c5cbc67fb322aa02d
lang0909 <lang0909@ajou.ac.kr>
2019-07-03 16:56:55 +0900

    add scanner

--- calculator.java
+++ calculator.java
@@ -1,7 +1,11 @@
+java.util.Scanner;
+
 public class calculator
 {
 	public static void main(String argv[])
 	{
-		
+		Scanner scan = new Scanner(System.in);
+		double ab;
+		ab = scan.nextDouble();
 	}
 }

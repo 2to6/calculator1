9a1adcc0dd8e41726f3f0062cbec5cb6e70b74e2
lang0909 <52433846+lang0909@users.noreply.github.com>
2019-07-03 17:13:10 +0900

    Add scanner (#1)
    * add scanner
    
    * modify scanner

--- calculator.java
+++ calculator.java
@@ -1,7 +1,13 @@
+java.util.Scanner;
+
 public class calculator
 {
 	public static void main(String argv[])
 	{
-		
+		Scanner scan = new Scanner(System.in);
+		double a;
+		dobule b;
+		a = scan.nextDouble();
+		b = scan.nextDouble();
 	}
 }
